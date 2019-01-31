bfcf29632ed36a39a034efdabf8b7fc88481e76f
peter sigurdson <peter@petersigurdson.net>
2019-01-31 16:34:10 -0500

    need to add the LookingForAstrilde Method

--- ConsoleApp39/Program.cs
+++ ConsoleApp39/Program.cs
@@ -12,9 +12,27 @@ namespace ConsoleApp39
         {
             Countryside Ontario = new Countryside();
             Ontario.run();
+
         }
     }
 
+    class LearningExample
+    {
+        public void run()
+        {
+            Village Toronto;
+            Village a;
+            Village b;
+            Toronto = new Village();
+            a = Toronto;
+
+            Toronto = new Village();
+            b = Toronto;
+
+            if (a == b) { Console.WriteLine("same"); } else 
+            {Console.WriteLine("different obj refs"); }
+        }
+    }
     class Village
     {
         public static int numberOfVillages = 0;
@@ -34,45 +52,37 @@ namespace ConsoleApp39
         public Village Ajax;
         public Village Head;
         public Village Tail;
-        public Village Temp;
+        public Village Current;
 
         public void run()
         {
             this.MapInitializer();
             this.LookForAstrilde();
+            Console.WriteLine("Hugi found Astrilde in " + Current.VillageName);
         }
 
         public void MapInitializer()
         {
+            Ajax = new Village();
+            Toronto = new Village();
             Maple = new Village();
+
             Maple.VillageName = "Maple";
             Maple.previousVillage = null;
             Maple.nextVillage = Toronto;
-            Maple.isAstrildeHere = true;
-            Toronto = new Village();
+            Ajax.isAstrildeHere = true;
+
             Toronto.previousVillage = Maple;
             Toronto.VillageName = "Toronto";
             Toronto.nextVillage = Ajax;
-            Ajax = new Village();
+            
             Ajax.VillageName = "Ajax";
             Ajax.nextVillage = null;
             Ajax.previousVillage = Toronto;
-            //Ajax.isAstrildeHere = true;
+
         }
 
-        public void LookForAstrilde() { 
         
-            Head = Maple;
-            if (Head.isAstrildeHere)
-            {
-                Console.WriteLine("Yeah ! Astrilde is in " + Head.VillageName);
-            }
-
-                //while (true)
-                //{
-
-                //}
-            }
         }
     
 }
